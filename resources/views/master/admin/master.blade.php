6511f003f82c268071dbcde1b0a6e7fa57ff2e08
Mossammat Faria <82267697+faria567@users.noreply.github.com>
2023-09-02 02:44:37 +0600

    Update master.blade.php

--- resources/views/master/admin/master.blade.php
+++ resources/views/master/admin/master.blade.php
@@ -388,7 +388,7 @@
                     </div>
                     <div class="col-sm-6">
                         <div class="text-sm-right d-none d-sm-block">
-                            Design & Develop by BASIS SEIP PHP B3
+                            
                         </div>
                     </div>
                 </div>
